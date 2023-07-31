f121cd8a263bd0002e9026cc61e685b2639fe92a
Crowdbotics <team@crowdbotics.com>
2023-07-31 06:16:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-waterfall-42538",
+  url: "https://bitter-waterfall-42538.botics.co",
+  api: "https://bitter-waterfall-42538.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
